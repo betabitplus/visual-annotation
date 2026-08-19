d242dc6e35c95beb324264999e5131a27971ecd4
betabitplus <8123085+betabitplus@users.noreply.github.com>
2026-08-19 03:04:46 +0300

    refactor: migrate visual-annotation to Ternforge

--- tests/visual_annotation/e2e/public_boundary/test_public_config_pipeline.py
+++ tests/visual_annotation/e2e/public_boundary/test_public_config_pipeline.py
@@ -9,7 +9,7 @@ Why:
 from __future__ import annotations
 
 import pytest
-from py_lib_tooling import console
+from py_lib_testkit import console
 
 from visual_annotation import VisualAnnotationConfig, get_config, install_config
 
